be00c9a3d8b40286c8c731d31496816b35bd1427
xjs <xjs14@mails.tsinghua.edu.cn>
2017-07-22 22:26:59 -0700

    Update prepro.py

--- tensorlayer/prepro.py
+++ tensorlayer/prepro.py
@@ -1608,6 +1608,8 @@ def data_augment(images,
         images = tf.map_fn(lambda img: random_transpose_image(img), images)
     if crop_size is not None:
         images = tf.map_fn(lambda img: random_crop_image(img, crop_size, crop_num), images)
+        shape = images.get_shape().as_list()
+        images = tf.reshape([-1] + shape[2:])
     
     return images
     
